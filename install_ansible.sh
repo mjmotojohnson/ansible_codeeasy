8d60b71f57f3d9a78a9872fc1b8762ff08c96256
Michael Johnson <mjmotojohnson@users.noreply.github.com>
2020-08-20 12:45:26 -0700

    Updated install instructions
    Updated installation per Updated Blog Post 4/29/2020
    - add installation of NetApp Collections
    - add options for updating NetApp Collections

--- install_ansible.sh
+++ install_ansible.sh
@@ -2,6 +2,7 @@
 ########################################
 # NetApp Getting Started with NetApp and Ansible - Install script
 # https://netapp.io/2018/10/08/getting-started-with-netapp-and-ansible-install-ansible/
+# - Updated Blog 4/29/2020
 #
 # Note: this install script works very well with NetApp Lab-on-Demand 
 #       IaaS OpenStack on Element and ONTAP v1.0
@@ -21,12 +22,19 @@ sudo pip install ansible
 #   Download latest versions of Ansible modules 
 #   this step is optional since the latest versions of Ansible ship with 
 #   the latest stable NetApp ONTAP and Element modules
-sudo pip install netapp-lib solidfire-sdk-python
+sudo pip install netapp-lib solidfire-sdk-python requests
+
+# Install NetApp collections and the collections directory to allow all users access
+sudo ansible-galaxy collection install netapp.ontap netapp.elementsw -p /usr/share/ansible/collection
+sudo chmod +rx /usr/share/ansible/collections
 
 # Install git repository package (probably already done)
 sudo yum install git -y
 
 # (Optional) Upgrade Ansible
 # sudo pip install ansible --upgrade
+
+# (Optional) Upgrade NetApp Galaxy Collections
+# sudo ansible-galaxy collection install netapp.ontap netapp.elementsw -p /usr/share/ansible/collections -f
 
 
